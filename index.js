204c1a058a55e74d3aebb5ce876f1c8ef1c90523
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-14 08:33:37 +0600

    Design Video Card & Display Dynamic Data commit

--- index.js
+++ index.js
@@ -22,33 +22,33 @@ for(let detelVoade of videos){
  const detailVedioCreatContainers =  document.createElement('div')
 
  detailVedioCreatContainers.innerHTML=`
-    <div id="detailVedioContainers" class="card bg-base-100 w-56 shadow-sm">
+ 
+   <div class="card bg-base-100 w-[200px] shadow-sm">
         <figure class="relative">
-        <p class="absolute bottom-0 right-0 bg-black text-white text-[12px] rounded-sm p-[2px] mr-2 mb-2">3hrs 56 minn ago</p>
-          <img
-            src="${detelVoade.thumbnail}" />
+          <img class="w-full h-[140px] object-cover"
+            src="${detelVoade.thumbnail}"
+            alt="Shoes" />
+            <p class="absolute text-white bg-black rounded-sm bottom-0 right-0 p-1 mr-2 mb-2 text-[10px]">3hrs 56 min ago</p>
         </figure>
         <div class="card-body">
-         <div class="flex gap-3">
-          <div class="avatar">
-            <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <div class="flex gap-3 items-center">
+                <div class="avatar h-6 w-6">
+                    <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+                      <img   src="${detelVoade.authors[0].profile_picture}" />
+                    </div>
+                  </div>
+                <h2 class="card-title">${detelVoade.title}</h2>
             </div>
-            </div>
-            <h2 class="card-title">${detelVoade.title}</h2>
-         </div>
+          
          <div class="flex">
-            <p class="">awlad hosain</p>
-           <span> <i class="fa-solid fa-certificate text-green-600"></i></span>
+            <p class="text-sm text-gray-400 flex gap-1 items-center">${detelVoade.authors[0].profile_name}<img class="w-3 h-3" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+
          </div>
-         <div>
-            <p>91k Views</p>
-         </div>
-          <!-- <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div> -->
+         <p class="text-sm text-gray-400">${detelVoade.others.views} views</p>
+
         </div>
       </div>
+   
  `;
  detailVedioContainer.appendChild(detailVedioCreatContainers)
 }
